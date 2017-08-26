8afe8f2cbe525c543fd9a522773fd743effc63ce
romanmarczewski <dharma@codemandala.com>
2017-08-26 13:34:38 +0200

    some polymorphic classes added

---
+++ src/com/zoo/animals/rare/SiberianTiger.java
@@ -0,0 +1,14 @@
+package com.zoo.animals.rare;
+
+import com.zoo.animals.Tiger;
+
+public class SiberianTiger extends Tiger {
+
+
+    @Override
+    public void makeSound() {
+        super.makeSound();
+        System.out.println("Siberian sound");
+
+    }
+}
